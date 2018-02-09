180c32d149ab7cc19f70aee0405117fd3e532c3d
HZ-7 <35828900+HZ-7@users.noreply.github.com>
2018-02-09 08:56:55 +0800

    Update pipelines.py

--- meizi_all/meizi_all/pipelines.py
+++ meizi_all/meizi_all/pipelines.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+# 项目管道文件
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -16,10 +16,13 @@ class MeiziAllPipeline(ImagesPipeline):
         yield scrapy.Request(imagsrc)
 
     def item_completed(self, results, item, info):
+#         创建分类文件夹
         if not os.path.exists(IMAGES_STORE + item['type']):
             os.mkdir(IMAGES_STORE + item['type'])
+#         创建图片标题文件夹
         if not os.path.exists(IMAGES_STORE +item['type']+'/'+ item['image_title']):
             os.mkdir(IMAGES_STORE +item['type']+'/'+ item['image_title'])
         image_path = [x['path'] for ok, x in results if ok]
+#         重命名图片
         os.rename(IMAGES_STORE + image_path[0], IMAGES_STORE +item['type']+'/'+ item['image_title']+ "/" + item['image_alt'] +"-"+ str(self.offset)+".jpg")
         self.offset += 1
